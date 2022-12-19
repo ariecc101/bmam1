30b50f7736ac22cdb0a7e7a8ef64dd1ff7da7887
Wisnu Setiadi <wisnuarisetiadi@gmail.com>
2022-12-19 10:29:43 +0000

    Add comment

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,20 @@
+#use image from docker hub node 14 alpine
 FROM node:14-alpine
+
+#set working directory to /app
 WORKDIR /app
+
+#copy all source code to working directory container
 COPY . .
+
+#env variable to container
 ENV NODE_ENV=production DB_HOST=item-db
+
+#install dependency and build app
 RUN npm install --production -unsafe-perm && npm run build
+
+#expose port 8080 in container
 EXPOSE 8080
+
+#command to run npm start
 CMD ["npm", "start"]
--- build_push_image.sh
+++ build_push_image.sh
@@ -1,11 +1,27 @@
 #!/bin/sh
+#build docker image from Dockerfile in current dir with name item-app tag v1
 docker build -t item-app:v1 .
+#add new line to tidy output
+echo '\n\n'
+
+#print list images docker
 docker images
+#add new line to tidy output
 echo '\n\n'
-docker tag item-app:v1 gchr
+
+#create new image tag
+docker tag item-app:v1 ghcr.io/ariecc101/item-app:v1
+#add new line to tidy output
 echo '\n\n'
-export GITHUB_PACKAGES_TOKEN=
-echo $GITHUB_PACKAGES_TOKEN | docker login gchr.io -u
+
+#export github token to env
+export GITHUB_PACKAGES_TOKEN=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
+#login to github packages
+echo $GITHUB_PACKAGES_TOKEN | docker login ghcr.io -u ariecc101 --password-stdin
+#add new line to tidy output
 echo '\n\n'
-docker push gchr.io
+
+#push image to github packages
+docker push ghcr.io/ariecc101/item-app:v1
+#add new line to tidy output
 echo '\n\n'
